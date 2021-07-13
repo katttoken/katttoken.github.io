cbbe00433bd3385897cbbffff93c9ef173633ecb
kitsudo1412 <kitsudo1412@gmail.com>
2021-07-13 09:08:46 +0700

    update

---
+++ css/main.js
@@ -0,0 +1,19 @@
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+// You can also use an ENS name for the contract address
+const kattAddress = "0x658dcfaeec83ab0bf6857b2603f94fc948ba86a9";
+
+// The ERC-20 Contract ABI, which is a common contract interface
+// for tokens (this is the Human-Readable ABI format)
+const kattAbi = [
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
+  "function balanceOf(address) view returns (uint)",
+  "function transfer(address to, uint amount)",
+  "event Transfer(address indexed from, address indexed to, uint amount)"
+];
+
+const kattContract = new ethers.Contract(kattAddress, kattAbi, provider);
+
+console.log(kattContract);
