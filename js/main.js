7f5f4a7079f329a79145eadbcac7bf5cab41ff14
kitsudo1412 <kitsudo1412@gmail.com>
2021-07-17 14:55:04 +0700

    gg

--- js/main.js
+++ js/main.js
@@ -11,18 +11,54 @@ const kattAddress = "0xc481676320d18c7459fa979128d0139d58c5f3cd";
 // The ERC-20 Contract ABI, which is a common contract interface
 // for tokens (this is the Human-Readable ABI format)
 const kattAbi = [
+  "function totalSupply() view returns (uint256)",
   "function name() view returns (string)",
   "function symbol() view returns (string)",
   "function balanceOf(address) view returns (uint)",
   "function transfer(address to, uint amount)",
+  "function approve(address delegate, uint numTokens) returns (bool)",
+  "function allowance(address owner, address delegate) view returns (uint)",
   "event Transfer(address indexed from, address indexed to, uint amount)",
-  "function getDayEmission() view returns (uint)"
+  "function transferFrom(address owner, address buyer, uint numTokens) returns (bool)",
+  "function joinLottery() returns(bool)",
+  "function withdrawAllLotteryWins(uint era) returns (uint value)",
+  "function withdrawLotteryWin(uint era, uint day) returns (uint value)",
+  "function getMemberLotteryWin(uint era, uint day, address member) view returns (uint value)",
+  "function getNextEraEmission() public view returns (uint)",
+  "function getDayEmission() view returns (uint)",
+  "function createBet(string calldata _description, bytes32[] calldata _options, uint256 _betDurationInHours) returns (uint256)",
+  "function placeBet(uint256 _betID, uint _option, uint256 _betAmount)",
+  "function endBet(uint256 _betID, uint winOption)",
+  "function withdrawBetWin(uint256 _betID) returns (uint value)",
+  "function getMemberBetWin(uint256 _betID, address _member) view returns (uint value)"
 ];
 
 const kattContract = new ethers.Contract(kattAddress, kattAbi, provider);
+
+// x = await kattContract.getDayEmission();
+// BigNumberToInt(x)
 
 async function connectWallet() {
   let provider;
   window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum));
   const signer = provider.getSigner();
+}
+
+function BigNumberToInt(x) {
+  return ethers.utils.formatEther(x);
+}
+
+$(function() {
+  getOverviewData();
+});
+
+function getOverviewData() {
+  $.getJSON("https://kattapi.tdao.me/getKattStatsFromEtherscan.php", function( data ) {
+    console.log(data);
+  });
+  $("#data-max-supply")
+
+  $.when(kattContract.totalSupply).then(function( data, textStatus, jqXHR ) {
+    $("#data-max-supply").html(data);
+  });
 }
